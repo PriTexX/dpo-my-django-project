d30d932a8aea6eae41c31933be2f2f27371bba7f
pritexx <bulavinvlad_2004@mail.ru>
2021-12-20 12:47:10 +0300

    Fixed an issue with requirements file

--- shopStore/shopStore/settings.py
+++ shopStore/shopStore/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,6 +142,8 @@ SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 STATICFILES_DIRS = (
     os.path.join(SITE_ROOT, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
