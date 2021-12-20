b3ace36ac3fb67337a3ed257497f117d636ccfc8
pritexx <bulavinvlad_2004@mail.ru>
2021-12-20 12:07:34 +0300

    Deploying

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn shopStore.wsgi --log-file -
--- shopStore/shopStore/settings.py
+++ shopStore/shopStore/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os, sys
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-&^dp-o775_28na5ofb^rrlwgfn%qni!p%4-pcv1^g9d_7)5&p6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -92,6 +93,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -130,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
